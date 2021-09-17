48b19cd50f470cf50171a5bf6ea3e4ce2252f7d0
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-09-16 19:23:25 -0500

    Check if content exists and increment occurrence

--- Labs/src/binarytree/Tree.java
+++ Labs/src/binarytree/Tree.java
@@ -4,33 +4,41 @@ public class Tree {
     private Node root;
     private int nodeCount;
 
-    public void add(String string) {
+    /**
+     * Adds content to the tree
+     * @param content String to add
+     */
+    public void add(String content) {
         // Prepare the new Node
-        Node newNode = new Node(string);
+        Node newNode = new Node(content);
         // Find a place for it on the tree
         if (root == null) {
             root = newNode; // there are no nodes
             nodeCount++;
         }
         else {
-            Node current = root; // Tree is not empty, find a place for newNode
-            boolean keepTraversing = true;
-            while (keepTraversing) {
-                if (newNode.getContent().compareTo(current.getContent()) < 0) {
-                    // left
-                    if (current.getLeft() == null) {
-                        current.setLeft(newNode);
-                        nodeCount++;
-                        keepTraversing = false;
-                    } else current = current.getLeft();
-                } else if (newNode.getContent().compareTo(current.getContent()) > 0) {
-                    // right
-                    if (current.getRight() == null) {
-                        current.setRight(newNode);
-                        nodeCount++;
-                        keepTraversing = false;
-                    } else current = current.getRight();
-                } else keepTraversing = false; // contents are equal
+            Node current = root; // Tree is not empty, find a place for this new node
+            Node existing = get(content); // Retrieve the node of content if it exists
+
+            // If the content already exists in the tree, increment the node
+            if (existing != null) existing.addOccurrence();
+            else { // content does not exist in the tree, add it
+                boolean keepTraversing = true;
+                while (keepTraversing) {
+                    if (newNode.getContent().compareTo(current.getContent()) < 0) { // left
+                        if (current.getLeft() == null) {
+                            current.setLeft(newNode);
+                            nodeCount++;
+                            keepTraversing = false;
+                        } else current = current.getLeft();
+                    } else if (newNode.getContent().compareTo(current.getContent()) > 0) { // right*
+                        if (current.getRight() == null) {
+                            current.setRight(newNode);
+                            nodeCount++;
+                            keepTraversing = false;
+                        } else current = current.getRight();
+                    } else keepTraversing = false; // contents are equal
+                }
             }
         }
 
