e79f1e3c2e5a8d22f6b5a2230f68494e0403b5d8
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-09-16 19:20:48 -0500

    Added annotations

--- Labs/src/binarytree/Node.java
+++ Labs/src/binarytree/Node.java
@@ -5,28 +5,53 @@ public class Node {
     private Node right;
     private Node left;
 
+    /**
+     * Constructor
+     * @param content Content to store in this node
+     */
     public Node(String content) {
         this.content = content;
         this.right = null;
         this.left = null;
+        occurances = 1;
     }
 
+    /**
+     * Returns the content of this Node
+     * @return String
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Returns the right node
+     * @return Node
+     */
     public Node getRight() {
         return right;
     }
 
+    /**
+     * Sets the right node
+     * @param right Node
+     */
     public void setRight(Node right) {
         this.right = right;
     }
 
+    /**
+     * Returns the left node
+     * @return Node
+     */
     public Node getLeft() {
         return left;
     }
 
+    /**
+     * Sets the left node
+     * @param left Node
+     */
     public void setLeft(Node left) {
         this.left = left;
     }
