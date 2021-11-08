ebb384757b935537addc9e1403075d11ba292843
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-11-08 15:26:00 -0600

    Implementation classes

---
+++ Labs/src/stacksqueues/QueueImplementation.java
@@ -0,0 +1,23 @@
+package stacksqueues;
+
+public class QueueImplementation {
+    public static void main(String[] args) {
+        Queue ourFirstQueue = new Queue(4);
+        ourFirstQueue.enqueue("A");
+        ourFirstQueue.enqueue("B");
+        ourFirstQueue.enqueue("C");
+        ourFirstQueue.enqueue("D");
+        ourFirstQueue.enqueue("E");
+        System.out.println(ourFirstQueue);
+        ourFirstQueue.dequeue();
+        System.out.println(ourFirstQueue);
+        ourFirstQueue.dequeue();
+        System.out.println(ourFirstQueue);
+        ourFirstQueue.dequeue();
+        System.out.println(ourFirstQueue);
+        ourFirstQueue.dequeue();
+        System.out.println(ourFirstQueue);
+        ourFirstQueue.dequeue();
+        System.out.println(ourFirstQueue);
+    }
+}
